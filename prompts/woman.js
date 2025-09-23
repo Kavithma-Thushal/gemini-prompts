9a22f8094452ef8b6c36b0a868a337cda87e4229
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2025-09-23 23:06:41 +0530

    sample images added

--- prompts/woman.js
+++ prompts/woman.js
@@ -1,6 +1,6 @@
 const woman_prompts = [
     {
-        text: "Create a retro vintage grainy but bright image of the reference picture but draped in a perfect red wine color Pinteresty aesthetic retro shirt with white pant and holding a rose flower in hands. It must feel like a 90s movie and romanticising windy environment. The girl is standing against a solid wall deep shadows and contrast drama, creating a mysterious and artistic atmosphere where the lighting is warm with a golden tones of evoking a sunset or golden hour glow.",
+        prompt: "Create a retro vintage grainy but bright image of the reference picture but draped in a perfect red wine color Pinteresty aesthetic retro shirt with white pant and holding a rose flower in hands. It must feel like a 90s movie and romanticising windy environment. The girl is standing against a solid wall deep shadows and contrast drama, creating a mysterious and artistic atmosphere where the lighting is warm with a golden tones of evoking a sunset or golden hour glow.",
         images: [
             "Samples/woman_prompt_1.png",
             "Samples/woman_prompt_1.png",
@@ -8,7 +8,7 @@ const woman_prompts = [
         ]
     },
     {
-        text: "A cinematic, ultra realistic portrait of a young woman in ann off-shoulder black outfit holding a single red rose she gazes slightly upward with a dreamy thoughtful expression soft circular spotlight on the wall behind her creates dramatic and a stylish editorial mood. ultra HD sharp details cinematic fashionpl photography aesthetic Aspect ratio: 9:16 (vertical)",
+        prompt: "A cinematic, ultra realistic portrait of a young woman in ann off-shoulder black outfit holding a single red rose she gazes slightly upward with a dreamy thoughtful expression soft circular spotlight on the wall behind her creates dramatic and a stylish editorial mood. ultra HD sharp details cinematic fashionpl photography aesthetic Aspect ratio: 9:16 (vertical)",
         images: [
             "Samples/woman_prompt_2.png",
             "Samples/woman_prompt_2.png",
@@ -16,7 +16,7 @@ const woman_prompts = [
         ]
     },
     {
-        text: "This is a photo of me. Craft a moody studio portrait of the uploaded person, bathed in golden-orange spotlight that creates a glowing circular halo behind them on the wall. The warm light should sculpt the face and upper body with soft, sunset-like tones, while casting a strong head shadow to the right. Style the person. Her eyes are closed and chin tilted slightly up.",
+        prompt: "This is a photo of me. Craft a moody studio portrait of the uploaded person, bathed in golden-orange spotlight that creates a glowing circular halo behind them on the wall. The warm light should sculpt the face and upper body with soft, sunset-like tones, while casting a strong head shadow to the right. Style the person. Her eyes are closed and chin tilted slightly up.",
         images: [
             "Samples/woman_prompt_3.png",
             "Samples/woman_prompt_3.png",
@@ -24,7 +24,7 @@ const woman_prompts = [
         ]
     },
     {
-        text: "Create a photo of the above image of a young woman that looking to the camera with solid beige background. She was sitting on a wooden stool. Copy the facial features. Make it more realistic. She is wearing a 3 inch white heels. The woman has waist length long wavy hair, that cascade over her shoulder and down her back. She is wearing a white off-shoulder crop top and denim baggy jeans.The lighting casts a soft shadow on the wall behind her. Her expression is serene and peaceful, The mood of the photo is soft and celebratory.",
+        prompt: "Create a photo of the above image of a young woman that looking to the camera with solid beige background. She was sitting on a wooden stool. Copy the facial features. Make it more realistic. She is wearing a 3 inch white heels. The woman has waist length long wavy hair, that cascade over her shoulder and down her back. She is wearing a white off-shoulder crop top and denim baggy jeans.The lighting casts a soft shadow on the wall behind her. Her expression is serene and peaceful, The mood of the photo is soft and celebratory.",
         images: [
             "Samples/woman_prompt_4.png",
             "Samples/woman_prompt_4.png",
@@ -32,7 +32,7 @@ const woman_prompts = [
         ]
     },
     {
-        text: "Create a retro vintage grainy but bright image of the reference picture but draped in a perfect brown color Pinterest aesthetic retro saree. It must feel like a 90s strands of hair on the face and a flower and her curls and romanticising windy environment. The girls is standing against a solid wall deep shadows and contrast drama, creating a mysterious sand artistic atmosphere where the lighting is warm with a golden tones of evoking a sunset or golden hour glow. The background is minimalist and slightly textured the expression on her face is moody, calm yet happy and introspective.",
+        prompt: "Create a retro vintage grainy but bright image of the reference picture but draped in a perfect brown color Pinterest aesthetic retro saree. It must feel like a 90s strands of hair on the face and a flower and her curls and romanticising windy environment. The girls is standing against a solid wall deep shadows and contrast drama, creating a mysterious sand artistic atmosphere where the lighting is warm with a golden tones of evoking a sunset or golden hour glow. The background is minimalist and slightly textured the expression on her face is moody, calm yet happy and introspective.",
         images: [
             "Samples/woman_prompt_5.png",
             "Samples/woman_prompt_5.png",
@@ -40,7 +40,7 @@ const woman_prompts = [
         ]
     },
     {
-        text: "Create a stylish woman of this uploaded photo, wearing a black halter top and light wash jeans, sitting casually on a wooden crate. She is looking in front and her left arm holding a denim jacket. The background has a smoke effect and bright black studio setting.",
+        prompt: "Create a stylish woman of this uploaded photo, wearing a black halter top and light wash jeans, sitting casually on a wooden crate. She is looking in front and her left arm holding a denim jacket. The background has a smoke effect and bright black studio setting.",
         images: [
             "Samples/woman_prompt_6.png",
             "Samples/woman_prompt_6.png",
@@ -48,7 +48,7 @@ const woman_prompts = [
         ]
     },
     {
-        text: "Transform this uploaded selfie into a premium-looking bathroom selfie.the bathroom should appear contemporary with polished tiles, good lighting, and mirror in the background. The person most be holding original i phone 17 pro max (second picture), meking the phone stand out as reflective,bold, and high end. The selfie should feel natural but with a touch of elegance, keeping the focus on the person and the i phone while the background reaming non-distracting.",
+        prompt: "Transform this uploaded selfie into a premium-looking bathroom selfie.the bathroom should appear contemporary with polished tiles, good lighting, and mirror in the background. The person most be holding original i phone 17 pro max (second picture), meking the phone stand out as reflective,bold, and high end. The selfie should feel natural but with a touch of elegance, keeping the focus on the person and the i phone while the background reaming non-distracting.",
         images: [
             "Samples/woman_prompt_7.png",
             "Samples/woman_prompt_7.png",
@@ -56,7 +56,7 @@ const woman_prompts = [
         ]
     },
     {
-        text: "Photo of the same person with the exact same face and hairstyle from the uploaded reference photo, sitting gracefully in a small wooden boat surrounded by lotus flowers and lily pads on a calm lake. The person wears a traditional white kurtha outfit, gently touching the pink lotus flowers with both hands. Natural lighting, serene and poetic atmosphere, high-resolution realistic photography.",
+        prompt: "Photo of the same person with the exact same face and hairstyle from the uploaded reference photo, sitting gracefully in a small wooden boat surrounded by lotus flowers and lily pads on a calm lake. The person wears a traditional white kurtha outfit, gently touching the pink lotus flowers with both hands. Natural lighting, serene and poetic atmosphere, high-resolution realistic photography.",
         images: [
             "Samples/woman_prompt_8.png",
             "Samples/woman_prompt_8.png",
