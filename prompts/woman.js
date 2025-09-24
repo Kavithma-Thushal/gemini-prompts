34019cb4afa6e8704abcdde0505dea376bdd2de7
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2025-09-24 14:08:22 +0530

    changed 2

--- prompts/woman.js
+++ prompts/woman.js
@@ -2,13 +2,29 @@ const woman_prompts = [
     {
         prompt: "A cinematic, ultra realistic portrait of a young woman in ann off-shoulder black outfit holding a single red rose she gazes slightly upward with a dreamy thoughtful expression soft circular spotlight on the wall behind her creates dramatic and a stylish editorial mood. ultra HD sharp details cinematic fashionpl photography aesthetic Aspect ratio: 9:16 (vertical)",
         images: [
+            "Samples/woman_prompt_1.1.png",
+            "Samples/woman_prompt_1.2.png",
+            "Samples/woman_prompt_1.3.png",
+        ]
+    },
+    {
+        prompt: "Create a photo of the above image of a young woman that looking to the camera with solid beige background. She was sitting on a wooden stool. Copy the facial features. Make it more realistic. She is wearing a 3 inch white heels. The woman has waist length long wavy hair, that cascade over her shoulder and down her back. She is wearing a white off-shoulder crop top and denim baggy jeans.The lighting casts a soft shadow on the wall behind her. Her expression is serene and peaceful, The mood of the photo is soft and celebratory.",
+        images: [
             "Samples/woman_prompt_2.1.png",
             "Samples/woman_prompt_2.2.png",
             "Samples/woman_prompt_2.3.png",
         ]
     },
     {
-        prompt: "Create a photo of the above image of a young woman that looking to the camera with solid beige background. She was sitting on a wooden stool. Copy the facial features. Make it more realistic. She is wearing a 3 inch white heels. The woman has waist length long wavy hair, that cascade over her shoulder and down her back. She is wearing a white off-shoulder crop top and denim baggy jeans.The lighting casts a soft shadow on the wall behind her. Her expression is serene and peaceful, The mood of the photo is soft and celebratory.",
+        prompt: "Create a retro vintage grainy but bright image of the reference picture but draped in a perfect brown color Pinterest aesthetic retro saree. It must feel like a 90s strands of hair on the face and a flower and her curls and romanticising windy environment. The girls is standing against a solid wall deep shadows and contrast drama, creating a mysterious sand artistic atmosphere where the lighting is warm with a golden tones of evoking a sunset or golden hour glow. The background is minimalist and slightly textured the expression on her face is moody, calm yet happy and introspective.",
+        images: [
+            "Samples/woman_prompt_3.1.png",
+            "Samples/woman_prompt_3.2.png",
+            "Samples/woman_prompt_3.3.png",
+        ]
+    },
+    {
+        prompt: "Create a retro vintage grainy but bright image of the reference picture but draped in a perfect red wine color Pinteresty aesthetic retro shirt with white pant and holding a rose flower in hands. It must feel like a 90s movie and romanticising windy environment. The girl is standing against a solid wall deep shadows and contrast drama, creating a mysterious and artistic atmosphere where the lighting is warm with a golden tones of evoking a sunset or golden hour glow.",
         images: [
             "Samples/woman_prompt_4.1.png",
             "Samples/woman_prompt_4.2.png",
@@ -16,27 +32,11 @@ const woman_prompts = [
         ]
     },
     {
-        prompt: "Create a retro vintage grainy but bright image of the reference picture but draped in a perfect brown color Pinterest aesthetic retro saree. It must feel like a 90s strands of hair on the face and a flower and her curls and romanticising windy environment. The girls is standing against a solid wall deep shadows and contrast drama, creating a mysterious sand artistic atmosphere where the lighting is warm with a golden tones of evoking a sunset or golden hour glow. The background is minimalist and slightly textured the expression on her face is moody, calm yet happy and introspective.",
+        prompt: "This is a photo of me. Craft a moody studio portrait of the uploaded person, bathed in golden-orange spotlight that creates a glowing circular halo behind them on the wall. The warm light should sculpt the face and upper body with soft, sunset-like tones, while casting a strong head shadow to the right. Style the person. Her eyes are closed and chin tilted slightly up.",
         images: [
             "Samples/woman_prompt_5.1.png",
             "Samples/woman_prompt_5.2.png",
             "Samples/woman_prompt_5.3.png",
-        ]
-    },
-    {
-        prompt: "Create a retro vintage grainy but bright image of the reference picture but draped in a perfect red wine color Pinteresty aesthetic retro shirt with white pant and holding a rose flower in hands. It must feel like a 90s movie and romanticising windy environment. The girl is standing against a solid wall deep shadows and contrast drama, creating a mysterious and artistic atmosphere where the lighting is warm with a golden tones of evoking a sunset or golden hour glow.",
-        images: [
-            "Samples/woman_prompt_1.1.png",
-            "Samples/woman_prompt_1.2.png",
-            "Samples/woman_prompt_1.3.png",
-        ]
-    },
-    {
-        prompt: "This is a photo of me. Craft a moody studio portrait of the uploaded person, bathed in golden-orange spotlight that creates a glowing circular halo behind them on the wall. The warm light should sculpt the face and upper body with soft, sunset-like tones, while casting a strong head shadow to the right. Style the person. Her eyes are closed and chin tilted slightly up.",
-        images: [
-            "Samples/woman_prompt_3.1.png",
-            "Samples/woman_prompt_3.2.png",
-            "Samples/woman_prompt_3.3.png",
         ]
     },
     {
