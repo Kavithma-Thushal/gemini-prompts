b1b98f8a512e48cde8a42e2844c89bfb6d8298c1
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2025-09-24 15:25:41 +0530

    refactored 2

--- prompts/man.js
+++ prompts/man.js
@@ -2,41 +2,41 @@ const man_prompts = [
     {
         prompt: "A hyper-realistic cinematic editorial portrait of the uploaded person (preserve face 100%). He stands tall in a dark moody studio, surrounded by soft drifting smoke under a dramatic spotlight. Outfit: Oversized maroon luxury suit with wide-leg trousers, paired with a slightly unbuttoned white silk shirt. The body is slim, but the suit remains oversized for a bold fashion-forward look. Both hands tucked casually in pockets, shoulders relaxed, confident expression, head tilted slightly upward. His face has a subtle glow, and he wears a thin gold necklace. The studio room background has a dark maroon tone to match the suit color, enhancing the atmosphere.",
         images: [
-            "Samples/man_prompt_1.1.png",
-            "Samples/man_prompt_1.2.jpg",
-            "Samples/man_prompt_1.3.png",
+            "samples/man_prompt_1.1.png",
+            "samples/man_prompt_1.2.jpg",
+            "samples/man_prompt_1.3.png",
         ]
     },
     {
         prompt: "Create a retro vintage grainy but bright image of the reference picture but draped in a perfect white colour suit Pinteresty aesthetic retro pants. It must feel like a '90s movie hair baddie with a small flower bookey in hand and romanticising a windy environment. The man is standing against a solid deep shadow and contrast drama, creating a mysterious and artistic atmosphere where the lighting is warm with golden tones evoking a sunset or golden hour glow.",
         images: [
-            "Samples/man_prompt_2.1.png",
-            "Samples/man_prompt_2.2.png",
-            "Samples/man_prompt_2.3.png",
+            "samples/man_prompt_2.1.png",
+            "samples/man_prompt_2.2.png",
+            "samples/man_prompt_2.3.png",
         ]
     },
     {
         prompt: "Cinematic 3:4 shot of me standing still on a rainy city sidewalk. I'm wearing an oversized black T-shirt, baggy pants, and sneakers. Holding an umbrella with one hand while rain pours heavily around me. Moody lighting, shallow depth of field, reflections on wet pavement, soft backlight halo. Film still look, slightly desaturated colors. Focus on emotion, solitude in the storm.",
         images: [
-            "Samples/man_prompt_3.1.png",
-            "Samples/man_prompt_3.2.png",
-            "Samples/man_prompt_3.3.png",
+            "samples/man_prompt_3.1.png",
+            "samples/man_prompt_3.2.png",
+            "samples/man_prompt_3.3.png",
         ]
     },
     {
         prompt: "Ultra-realistic 8K cinematic portrait of indoors, leaning against a light wall in a luxurious setting, wearing sunglasses a black Balmain hooded robe with gold logo print, matching belt, and silver pendant necklace, holding a patterned mug and checking his phone with a thoughtful gaze, styled hair slightly messy, dramatic warm highlights and soft shadows for a magazine-editorial look.",
         images: [
-            "Samples/man_prompt_4.1.png",
-            "Samples/man_prompt_4.2.jpg",
-            "Samples/man_prompt_4.3.png",
+            "samples/man_prompt_4.1.png",
+            "samples/man_prompt_4.2.jpg",
+            "samples/man_prompt_4.3.png",
         ]
     },
     {
         prompt: "Stylish young years 25 Boy in a sleek all-black outfit posing confidently against a dark background.He is wearing a fitted black suit with a black shirt slightly unbuttoned, revealing a thin silver necklace with a small geometric pendant. He has a modern, voluminous hairstyle and wears dark round sunglasses. The lighting is dramatic, highlighting his sharp jawline and smooth skin, with a moody, high-fashion editorial vibe. (Make sure same face as in picture don't change it)",
         images: [
-            "Samples/man_prompt_5.1.png",
-            "Samples/man_prompt_5.2.png",
-            "Samples/man_prompt_5.3.png",
+            "samples/man_prompt_5.1.png",
+            "samples/man_prompt_5.2.png",
+            "samples/man_prompt_5.3.png",
         ]
     },
 ];
--- prompts/woman.js
+++ prompts/woman.js
@@ -2,65 +2,65 @@ const woman_prompts = [
     {
         prompt: "A cinematic, ultra realistic portrait of a young woman in ann off-shoulder black outfit holding a single red rose she gazes slightly upward with a dreamy thoughtful expression soft circular spotlight on the wall behind her creates dramatic and a stylish editorial mood. ultra HD sharp details cinematic fashionpl photography aesthetic Aspect ratio: 9:16 (vertical)",
         images: [
-            "Samples/woman_prompt_1.1.png",
-            "Samples/woman_prompt_1.2.png",
-            "Samples/woman_prompt_1.3.png",
+            "samples/woman_prompt_1.1.png",
+            "samples/woman_prompt_1.2.png",
+            "samples/woman_prompt_1.3.png",
         ]
     },
     {
         prompt: "Create a photo of the above image of a young woman that looking to the camera with solid beige background. She was sitting on a wooden stool. Copy the facial features. Make it more realistic. She is wearing a 3 inch white heels. The woman has waist length long wavy hair, that cascade over her shoulder and down her back. She is wearing a white off-shoulder crop top and denim baggy jeans.The lighting casts a soft shadow on the wall behind her. Her expression is serene and peaceful, The mood of the photo is soft and celebratory.",
         images: [
-            "Samples/woman_prompt_2.1.png",
-            "Samples/woman_prompt_2.2.png",
-            "Samples/woman_prompt_2.3.png",
+            "samples/woman_prompt_2.1.png",
+            "samples/woman_prompt_2.2.png",
+            "samples/woman_prompt_2.3.png",
         ]
     },
     {
         prompt: "Create a retro vintage grainy but bright image of the reference picture but draped in a perfect brown color Pinterest aesthetic retro saree. It must feel like a 90s strands of hair on the face and a flower and her curls and romanticising windy environment. The girls is standing against a solid wall deep shadows and contrast drama, creating a mysterious sand artistic atmosphere where the lighting is warm with a golden tones of evoking a sunset or golden hour glow. The background is minimalist and slightly textured the expression on her face is moody, calm yet happy and introspective.",
         images: [
-            "Samples/woman_prompt_3.1.png",
-            "Samples/woman_prompt_3.2.png",
-            "Samples/woman_prompt_3.3.png",
+            "samples/woman_prompt_3.1.png",
+            "samples/woman_prompt_3.2.png",
+            "samples/woman_prompt_3.3.png",
         ]
     },
     {
         prompt: "Create a retro vintage grainy but bright image of the reference picture but draped in a perfect red wine color Pinteresty aesthetic retro shirt with white pant and holding a rose flower in hands. It must feel like a 90s movie and romanticising windy environment. The girl is standing against a solid wall deep shadows and contrast drama, creating a mysterious and artistic atmosphere where the lighting is warm with a golden tones of evoking a sunset or golden hour glow.",
         images: [
-            "Samples/woman_prompt_4.1.png",
-            "Samples/woman_prompt_4.2.png",
-            "Samples/woman_prompt_4.3.png",
+            "samples/woman_prompt_4.1.png",
+            "samples/woman_prompt_4.2.png",
+            "samples/woman_prompt_4.3.png",
         ]
     },
     {
         prompt: "This is a photo of me. Craft a moody studio portrait of the uploaded person, bathed in golden-orange spotlight that creates a glowing circular halo behind them on the wall. The warm light should sculpt the face and upper body with soft, sunset-like tones, while casting a strong head shadow to the right. Style the person. Her eyes are closed and chin tilted slightly up.",
         images: [
-            "Samples/woman_prompt_5.1.png",
-            "Samples/woman_prompt_5.2.png",
-            "Samples/woman_prompt_5.3.png",
+            "samples/woman_prompt_5.1.png",
+            "samples/woman_prompt_5.2.png",
+            "samples/woman_prompt_5.3.png",
         ]
     },
     {
         prompt: "Create a stylish woman of this uploaded photo, wearing a black halter top and light wash jeans, sitting casually on a wooden crate. She is looking in front and her left arm holding a denim jacket. The background has a smoke effect and bright black studio setting.",
         images: [
-            "Samples/woman_prompt_6.1.png",
-            "Samples/woman_prompt_6.2.png",
-            "Samples/woman_prompt_6.3.png",
+            "samples/woman_prompt_6.1.png",
+            "samples/woman_prompt_6.2.png",
+            "samples/woman_prompt_6.3.png",
         ]
     },
     {
         prompt: "Transform this uploaded selfie into a premium-looking bathroom selfie.the bathroom should appear contemporary with polished tiles, good lighting, and mirror in the background. The person most be holding original i phone 17 pro max (second picture), meking the phone stand out as reflective,bold, and high end. The selfie should feel natural but with a touch of elegance, keeping the focus on the person and the i phone while the background reaming non-distracting.",
         images: [
-            "Samples/woman_prompt_7.1.png",
-            "Samples/woman_prompt_7.2.png",
-            "Samples/woman_prompt_7.3.png",
+            "samples/woman_prompt_7.1.png",
+            "samples/woman_prompt_7.2.png",
+            "samples/woman_prompt_7.3.png",
         ]
     },
     {
         prompt: "Photo of the same person with the exact same face and hairstyle from the uploaded reference photo, sitting gracefully in a small wooden boat surrounded by lotus flowers and lily pads on a calm lake. The person wears a traditional white kurtha outfit, gently touching the pink lotus flowers with both hands. Natural lighting, serene and poetic atmosphere, high-resolution realistic photography.",
         images: [
-            "Samples/woman_prompt_8.1.png",
-            "Samples/woman_prompt_8.2.png",
-            "Samples/woman_prompt_8.3.png",
+            "samples/woman_prompt_8.1.png",
+            "samples/woman_prompt_8.2.png",
+            "samples/woman_prompt_8.3.png",
         ]
     },
 ];
